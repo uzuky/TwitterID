363909538886c1b0371b853776d065187b2cff7a
uzuky <uzuky04@gmail.com>
2014-09-01 05:12:22 +0900

    ₍₍ ◝('ω')◟ ⁾⁾

--- TwitterID.js
+++ TwitterID.js
@@ -4,7 +4,7 @@ javascript:(function () {
     H = D.documentElement.innerHTML,
     I = D.createElement('div');
   var V = 'v20140901',
-    UA, PF, SR, ID;
+    UA, PF, SR, ID, YO;
   if (L.match(/^https:\/\/twitter\.com\/[0-9a-zA-Z_]+$/)) {
     UA = D.getElementsByClassName("UserActions"),
     SR = UA[0].innerHTML,
@@ -15,18 +15,21 @@ javascript:(function () {
     ID = SR.replace(/(?:.|\n|\r|\u2028|\u2029)+data-id="([0-9]+)"(?:.|\n|\r|\u2028|\u2029)+/g, '$1');
   } else {
     alert('ユーザーのトップページで実行してね\n' + V);
+    YO = NO;
   }
-  I.innerHTML = '<div><b><span title="閉じる" onClick="var c = &quot;numeric-id&quot;,close = document.getElementById(c);close.parentNode.removeChild(close);" style="float:right; cursor:pointer; font-size:large; position:relative; bottom:10px; left:5px; color:#FFF;">×</span>UserID</b></div><input type="text" value="https://twitter.com/intent/user?user_id=' + ID + '" readonly="true" onclick="this.select();"><br><input type="text" value="' + ID + '" readonly="true" onclick="this.select();"><br><input type="button" value="開く" onClick="window.open(\'https://twitter.com/intent/user?user_id=' + ID + '\',\'_blank\',\'width=650,height=450\');"><br><small>' + V + '</small>';
-  I.style.color = 'rgb(0,0,0)';
-  I.style.textAlign = 'center';
-  I.style.padding = '10px';
-  I.style.position = 'fixed';
-  I.style.zIndex = '9999';
-  I.style.fontSize = '12px';
-  I.style.border = '2px solid black';
-  I.style.right = '20px';
-  I.style.top = '50px';
-  I.style.background = 'rgba(128,128,128,0.5)';
-  I.setAttribute("id", "numeric-id");
-  document.body.appendChild(I);
+  if (YO != 'NO') {
+    I.innerHTML = '<div><b><span title="閉じる" onClick="var c = &quot;numeric-id&quot;,close = document.getElementById(c);close.parentNode.removeChild(close);" style="float:right; cursor:pointer; font-size:large; position:relative; bottom:10px; left:5px; color:#FFF;">×</span>UserID</b></div><input type="text" value="https://twitter.com/intent/user?user_id=' + ID + '" readonly="true" onclick="this.select();"><br><input type="text" value="' + ID + '" readonly="true" onclick="this.select();"><br><input type="button" value="開く" onClick="window.open(\'https://twitter.com/intent/user?user_id=' + ID + '\',\'_blank\',\'width=650,height=450\');"><br><small>' + V + '</small>';
+    I.style.color = 'rgb(0,0,0)';
+    I.style.textAlign = 'center';
+    I.style.padding = '10px';
+    I.style.position = 'fixed';
+    I.style.zIndex = '9999';
+    I.style.fontSize = '12px';
+    I.style.border = '2px solid black';
+    I.style.right = '20px';
+    I.style.top = '50px';
+    I.style.background = 'rgba(128,128,128,0.5)';
+    I.setAttribute("id", "numeric-id");
+    document.body.appendChild(I);
+  }
  })();
